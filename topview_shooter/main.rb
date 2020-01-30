3461765d5db21928ad82aef7d277c745fd740c1d
ongaeshi <ongaeshi0621@gmail.com>
2020-01-30 22:37:12 +0900

    feat: Enlarge and display a 128x128 screen

--- topview_shooter/main.rb
+++ topview_shooter/main.rb
@@ -3,18 +3,55 @@ include Raylib
 
 SCREEN_WIDTH = 128
 SCREEN_HEIGHT = 128
+SCALE = 3
 
-window(SCREEN_WIDTH, SCREEN_HEIGHT, "topview_shooter") do
-	set_target_fps(60)
+window(SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE, "topview_shooter") do
+  set_target_fps(30)
 
-	until window_should_close do
-		# Update
-		# TODO: Update your variables here
+	target = load_render_texture(SCREEN_WIDTH, SCREEN_HEIGHT)
+	
+	x = 0
 
-		# Draw
-		draw do
-			clear_background(RAYWHITE)
-			draw_text("Hello, World!", 190, 200, 20, LIGHTGRAY);
-		end
-	end
+  until window_should_close
+    # Update
+    # TODO: Update your variables here
+		x += 1
+		x = -80 if x > 150
+
+    # Draw
+    draw do
+			texture_mode(target) do
+				clear_background(Color.init(194, 195, 199, 255))
+				
+				(0...16).each do |i|
+					(0...16).each do |j|
+						if i % 2 == 0 || j % 2 == 0
+							draw_rectangle(i*8, j*8, 8, 8, Color.init(29, 43, 83, 255))
+						end
+					end
+				end
+
+				draw_text("Hello, World!", x, SCREEN_HEIGHT*0.5-5, 10, Color.init(255, 163, 0, 255))
+      end
+
+      draw_texture_pro(
+        target.texture,
+        Rectangle.init(
+          0.0,
+          0.0,
+          target.texture.width,
+          -target.texture.height
+        ),
+        Rectangle.init(
+          (get_screen_width() - SCREEN_WIDTH * SCALE) * 0.5,
+          (get_screen_height() - SCREEN_HEIGHT * SCALE) * 0.5,
+          SCREEN_WIDTH * SCALE,
+          SCREEN_HEIGHT * SCALE
+        ),
+        Vector2.init(0, 0),
+        0.0,
+        WHITE
+      )
+    end
+  end
 end
