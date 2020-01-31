05f49153b29041cce0fe9c48ad2216b8cd9cf3fe
ongaeshi <ongaeshi0621@gmail.com>
2020-02-01 00:34:18 +0900

    feat: Walk to read input

--- topview_shooter/main.rb
+++ topview_shooter/main.rb
@@ -2,14 +2,15 @@ require "../lib/raylib"
 require "../lib/pico"
 include Pico
 
-ipf = 8  # interval per frame
-nf = 2   # number of frames
+IPF = 8  # interval per frame
+NF = 2   # number of frames
 max_gun_interval = 8
 
 beam_spd = 4
 beam_len = 4
 beam_life = 64
 
+@@t = 0
 @@pc = nil
 
 class Character
@@ -26,10 +27,12 @@ class Character
   end
 
   def update
+    input
+
     # sprite
     @s = (@d - 1) + @s0
     if @walking
-      @s += flr((t % (nf * ipf)) / ipf + 1) * 16
+      @s += flr((@@t % (NF * IPF)) / IPF + 1) * 16
     end
     if self == @@pc and @gun
       @s += 4
@@ -42,6 +45,55 @@ class Character
         @x = nx
         @y = ny
       end
+    end
+  end
+
+  def input()
+    # walk
+    @walking = false
+    if btn(0)
+      @dx = -1
+      @dy = 0
+      @d = 1
+      @walking = true
+    end
+    if btn(1)
+      @dx = 1
+      @dy = 0
+      @d = 2
+      @walking = true
+    end
+    if btn(2)
+      @dx = 0
+      @dy = -1
+      @d = 3
+      @walking = true
+    end
+    if btn(3)
+      @dx = 0
+      @dy = 1
+      @d = 4
+      @walking = true
+    end
+    # gun
+    @gun = btn(4)
+    if @gun and @gun_interval == 0
+      bx = @x
+      by = @y
+      if @d == 1
+        bx -= 3
+        by += 1
+      elsif @d == 2
+        bx += 2
+        by += 1
+      elsif @d == 3
+        bx += 2
+        by -= 2
+      else
+        bx -= 3
+      end
+      add_beam(bx, by, @dx, @dy)
+      @gun_interval = max_gun_interval
     end
   end
 
@@ -68,7 +120,6 @@ end
 
 class Scene
   def initialize
-    @t = 0
     @objs = []
 
     @@pc = Player.new
@@ -76,7 +127,7 @@ class Scene
   end
 
   def update
-    @t += 1
+    @@t += 1
     @objs.each { |e| e.update }
   end
 
