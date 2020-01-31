325f4203279efd0449524ad15a6e19c8fd811e90
ongaeshi <ongaeshi0621@gmail.com>
2020-02-01 00:29:30 +0900

    feat: Add Player

--- topview_shooter/main.rb
+++ topview_shooter/main.rb
@@ -10,10 +10,69 @@ beam_spd = 4
 beam_len = 4
 beam_life = 64
 
+@@pc = nil
+
+class Character
+  def initialize(x, y, s0)
+    @x = x
+    @y = y
+    @s = s0
+    @s0 = s0
+    @d = 4
+    @dx = 0
+    @dy = 1
+    @spd = 2
+    @walking = false
+  end
+
+  def update
+    # sprite
+    @s = (@d - 1) + @s0
+    if @walking
+      @s += flr((t % (nf * ipf)) / ipf + 1) * 16
+    end
+    if self == @@pc and @gun
+      @s += 4
+    end
+    # position
+    if @walking
+      nx = @x + @dx * @spd
+      ny = @y + @dy * @spd
+      if self == @@pc # or not collide_with_other_npcs(c, nx, ny, 6)
+        @x = nx
+        @y = ny
+      end
+    end
+  end
+
+  def draw
+    spr(@s, @x - 4, @y - 4)
+  end
+end
+
+class Player < Character
+  def initialize
+    super(64, 64, 1)
+    @gun = false
+    @gun_interval = 0
+  end
+
+  def update
+    super()
+
+    if @gun_interval > 0
+      @gun_interval -= 1
+    end
+  end
+end
+
 class Scene
   def initialize
     @t = 0
     @objs = []
+
+    @@pc = Player.new
+    @objs << @@pc
   end
 
   def update
